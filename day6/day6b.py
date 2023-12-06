70b9b151d4888b2f923075e92e6a52734d2a9bbb
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-12-06 10:51:20 +0000

    Non-brute force 6b

--- day6/day6b.py
+++ day6/day6b.py
@@ -2,6 +2,7 @@
 # Brute force reduced by 1/2 as possibilities are a histogram
 
 import re
+import math
 
 with open('input.txt', 'r') as file:
   input_lines = [line.strip() for line in file]
@@ -10,18 +11,9 @@ nums = [''.join(re.findall(r'\d+', line)) for line in input_lines]
 
 race = (int(nums[0]), int(nums[1]))
 
-wins_count = 0
+# Solve via quadratic equation
 
-# Acts as a histogram so only check half and double the result
-half = int(race[0]/2)
-
-# No point doing 0 or max seconds as no movement
-for i in range(0,half+1):
-  res = i * (int(race[0]) - i)
-  multiplier = 2
-  # Exact middle not needing duplicating
-  if (i == half) and (i % 2 != 0):
-    multiplier = 1
-  if res > int(race[1]): wins_count += multiplier
-
-print(wins_count)
+tmp = math.sqrt(race[0]**2 - 4*race[1])
+c2 = (race[0] + tmp) / 2
+c1 = (race[0] - tmp) / 2
+print(int(c2) - int(math.ceil(c1)) + 1)
